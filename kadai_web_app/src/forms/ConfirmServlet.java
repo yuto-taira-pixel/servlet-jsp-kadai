a935a45a749d16683f37e01090d07b4c6232e097
yuto-taira-pixel <yuto-taira@gr.paramix.co.jp>
2026-04-21 14:02:11 +0900

    Add files via upload

---
+++ kadai_web_app/src/forms/ConfirmServlet.java
@@ -0,0 +1,36 @@
+package forms;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ConfirmServlet extends HttpServlet {
+    // POSTメソッドのリクエスト受信時に実行されるメソッド
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        // リクエスト・レスポンスの設定
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+
+        // JSPからのリクエストデータ取得
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String address = request.getParameter("address");
+        String phone_number = request.getParameter("phone_number");
+
+        // リクエストスコープにデータ保存
+        request.setAttribute( "name", name );
+        request.setAttribute( "email", email );
+        request.setAttribute( "address", address );
+        request.setAttribute( "phone_number", phone_number );
+
+        // フォワードによる画面遷移
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/pages/confirmPage.jsp");
+        dispatcher.forward(request, response);
+    }
+}
